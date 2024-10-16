db06eedd3af46bec8ad1befc9a3f19b4b41dccd8
shahid <kancain00@gmail.com>
2024-10-16 16:57:16 +0500

    intialize session

--- main.py
+++ main.py
@@ -11,7 +11,13 @@ st.title('AI Assisance for Data Science')
 st.write('Hello, Im your AI Assistant and I am here to help you with your Data Science Projects.')
 
 
+#Intialize the key n session state
+if 'clicked' not in st.session_state:
+    st.session_state.clicked={1:False}
 
+#function to update value in sessions
+def clicked(button):
+    st.session_state.clicked[button]=True
 
 if st.button("Let's get started"):
     st.header("Exploraty Data Analysis")
